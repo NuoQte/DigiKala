755a7e39c1f786d5f5a42c53e06a992856b6bac6
NuoQTe <nuoqte@gmail.com>
2024-06-02 03:17:45 +0330

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 from setuptools import setup, find_packages
 
 setup(
     name='DigiKala',
-    version='0.5',
+    version='1.0',
     packages=find_packages(),
     license='MIT',
     description='DigiKala library to connect and work with digikala.com!',
@@ -12,21 +11,5 @@ setup(
     author='NuoQTe',
     author_email='nuoqte@gmail.com',
     url='https://github.com/NuoQte/DigiKala',
-    install_requires=['aiohttp'],
-=======
-from setuptools import setup, find_packages
-
-setup(
-    name='DigiKala',
-    version='0.5',
-    packages=find_packages(),
-    license='MIT',
-    description='DigiKala library to connect and work with digikala.com!',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='NuoQTe',
-    author_email='nuoqte@gmail.com',
-    url='https://github.com/NuoQte/DigiKala',
-    install_requires=['aiohttp'],
->>>>>>> 4f4ca4e (initial commit)
+    install_requires=['aiohttp']
 )
